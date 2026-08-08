7e7d7a6f3f65cdd57f378ed2515e65798bff33da
Tuan <tuanlqtl00006@gmail.com>
2026-08-08 13:52:00 +0700

    Delete Downloads/ComputerStore_Full_updated directory

--- Downloads/ComputerStore_Full_updated/ComputerStore_Full/FrontEnd/src/api/brand.js
+++
@@ -1,8 +0,0 @@
-import api from "./axios";
-
-export const getBrands = () => api.get("/brands");
-export const getActiveBrands = () => api.get("/brands/active");
-export const getBrandById = (id) => api.get(`/brands/${id}`);
-export const createBrand = (data) => api.post("/brands", data);
-export const updateBrand = (id, data) => api.put(`/brands/${id}`, data);
-export const deleteBrand = (id) => api.delete(`/brands/${id}`);
--- Downloads/ComputerStore_Full_updated/ComputerStore_Full/FrontEnd/src/api/cpu.js
+++
@@ -1,8 +0,0 @@
-import api from "./axios";
-
-export const getCpus = () => api.get("/cpus");
-export const getActiveCpus = () => api.get("/cpus/active");
-export const getCpuById = (id) => api.get(`/cpus/${id}`);
-export const createCpu = (data) => api.post("/cpus", data);
-export const updateCpu = (id, data) => api.put(`/cpus/${id}`, data);
-export const deleteCpu = (id) => api.delete(`/cpus/${id}`);
