68fe4b8cf974e0b4a7f0dbf357fd7dc0e90a0400
Tuan <tuanlqtl00006@gmail.com>
2026-08-08 13:32:10 +0700

    Delete ComputerStore_Full directory

--- ComputerStore_Full/FrontEnd/src/api/category.js
+++
@@ -1,8 +0,0 @@
-import api from "./axios";
-
-export const getCategories = () => api.get("/categories");
-export const getActiveCategories = () => api.get("/categories/active");
-export const getCategoryById = (id) => api.get(`/categories/${id}`);
-export const createCategory = (data) => api.post("/categories", data);
-export const updateCategory = (id, data) => api.put(`/categories/${id}`, data);
-export const deleteCategory = (id) => api.delete(`/categories/${id}`);
--- ComputerStore_Full/FrontEnd/src/api/ram.js
+++
@@ -1,8 +0,0 @@
-import api from "./axios";
-
-export const getRams = () => api.get("/rams");
-export const getActiveRams = () => api.get("/rams/active");
-export const getRamById = (id) => api.get(`/rams/${id}`);
-export const createRam = (data) => api.post("/rams", data);
-export const updateRam = (id, data) => api.put(`/rams/${id}`, data);
-export const deleteRam = (id) => api.delete(`/rams/${id}`);
